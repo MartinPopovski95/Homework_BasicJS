1fd26743f89b9ef30c6ed50f31315707d742cf45
MartinPopovski95 <popovskimartin1995@gmail.com>
2024-12-04 21:56:17 +0100

    CLASS01 Homework

---
+++ CLASS01 Homework/totalPrice.js
@@ -0,0 +1,15 @@
+let onePhoneNet = 119.95
+console.log("Net price for 1 phone is:")
+console.log(onePhoneNet)
+
+let taxRate = 1.05
+console.log("Tax rate is 5%")
+console.log(taxRate)
+
+let onePhoneGross = onePhoneNet * taxRate
+console.log("Gross price per phone is:")
+console.log(onePhoneGross)
+
+finalResult = onePhoneGross * 30
+console.log("Total price for 30 phones is:")
+console.log(finalResult)
